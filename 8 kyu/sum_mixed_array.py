c3b453384c80b96ae497861b1e8abdea2a12a1f1
Andreas <38704712+andreasleszczynski@users.noreply.github.com>
2019-05-26 23:13:48 +0200

    Create sum_mixed_array.py
    8 kyu Kata Fundamentals

---
+++ 8 kyu/sum_mixed_array.py
@@ -0,0 +1,12 @@
+""" Sum Mixed Array - 8 kyu Kata Fundamentals
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. """
+def sum_mix(arr):
+    return sum(int(x) for x in arr)
+
+print(sum_mix([9, 3, '7', '3'])) # 22
+print(sum_mix(['5', '0', 9, 3, 2, 1, '9', 6, 7])) # 42
+print(sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])) # 41
+print(sum_mix(['1', '5', '8', 8, 9, 9, 2, '3'])) # 45
+print(sum_mix([8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7])) # 61
