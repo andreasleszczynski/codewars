1d5e5fae266709b2f78cc93be11fa859b60b8e38
Andreas <38704712+andreasleszczynski@users.noreply.github.com>
2019-05-27 22:41:34 +0200

    Create convert_number_to_string.py
    8 kyu Kata Fundamentals

---
+++ 8 kyu/convert_number_to_string.py
@@ -0,0 +1,15 @@
+""" Convert a Number to a String! 8 kyu Kata Fundamentals
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+number_to_string(123) /* returns '123' */
+number_to_string(999) /* returns '999' */
+"""
+
+def number_to_string(num):
+    return str(num)
+
+
+print(number_to_string(67)) # '67'
