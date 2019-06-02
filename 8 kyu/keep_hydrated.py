9411c9bca2fb1a2118566a590f4c88d386d7be74
Andreas <38704712+andreasleszczynski@users.noreply.github.com>
2019-06-02 21:10:21 +0200

    Create keep_hydrated.py
    8 kyu Kata Fundamentals

---
+++ 8 kyu/keep_hydrated.py
@@ -0,0 +1,25 @@
+""" Keep Hydrated! - 8 kyu Kata Fundamentals
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5
+litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres
+Nathan will drink, rounded to the smallest value.
+
+For example:
+time = 3 ----> litres = 1
+time = 6.7---> litres = 3
+time = 11.8--> litres = 5
+"""
+
+def litres(time):
+    return time//2
+
+print(litres(2)) # 1
+print(litres(1.4)) # 0
+print(litres(12.3)) # 6
+print(litres(0.82)) # 0
+print(litres(11.8)) # 5
+print(litres(1787)) # 893
+print(litres(0)) # 0
