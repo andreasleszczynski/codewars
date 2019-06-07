6e98f45c21053e1a61414b44e47ab957e0591e27
Andreas <38704712+andreasleszczynski@users.noreply.github.com>
2019-06-07 22:11:56 +0200

    Create square_sum.py
    8 kyu Kata Fundamentals

---
+++ 8 kyu/square_sum.py
@@ -0,0 +1,12 @@
+""" Square(n) Sum - 8 kyu Kata Fundamentals
+Complete the square sum function so that it squares each number passed into it
+and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9."""
+
+def square_sum(numbers):
+    return sum(n ** 2 for n in numbers)
+
+print(square_sum([1,2])) # 'squareSum did not return a value'
+print(square_sum([1,2])) # 5
+print(square_sum([0, 3, 4, 5])) # 50
