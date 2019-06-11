d0093ee53ca426509fa9908088924bfcca065943
Andreas <38704712+andreasleszczynski@users.noreply.github.com>
2019-06-11 22:16:39 +0200

    Create reversed_words.py
    8 kyu Kata Algorithms

---
+++ 8 kyu/reversed_words.py
@@ -0,0 +1,15 @@
+""" Reversed Words - 8 kyu Kata Algorithms
+Complete the solution so that it reverses all of the words within the string
+passed in.
+
+Example:
+
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+"""
+
+def reverseWords(str):
+    return " ".join(str.split(" ")[::-1])
+
+
+print(reverseWords("hello world!"), "world! hello")
