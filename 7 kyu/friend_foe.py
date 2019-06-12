c834e061f8c0927dc127b40b34ea2ef921bc271f
Andreas <38704712+andreasleszczynski@users.noreply.github.com>
2019-06-12 20:32:27 +0200

    Create friend_foe.py
    7 kyu Kata Fundamentals

---
+++ 7 kyu/friend_foe.py
@@ -0,0 +1,20 @@
+""" Friend or Foe? - 7 kyu Kata Fundamentals
+Make a program that filters a list of strings and returns a list with only your
+ friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a
+friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+"""
+
+def friend(x):
+    return [person for person in x if len(person)==4]
+
+
+
+print(friend(["Ryan", "Kieran", "Mark",]), ["Ryan", "Mark"])
